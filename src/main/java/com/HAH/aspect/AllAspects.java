a19968bfcf58d70f2ee2af02df51605b01039f72
hhtun137 <htunaunghlaing199@gmail.com>
2024-03-07 11:55:25 +0630

    writing pointcut method

--- src/main/java/com/HAH/aspect/AllAspects.java
+++ src/main/java/com/HAH/aspect/AllAspects.java
@@ -3,18 +3,23 @@ package com.HAH.aspect;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class AllAspects {
-	
-	@Before("bean(adminDao)")
+
+	@Pointcut("execution(* search*(..))")
+	public void doPointCut() {
+	}
+
+	@Before("doPointCut()")
 	public void beforeInvocation(JoinPoint joinPoint) {
 		System.out.println("Before Invocation");
 		System.out.println("--------------------");
-		System.out.printf("%-15s : %s%n".formatted("Target Class",joinPoint.getTarget().getClass().getSimpleName()));
-		System.out.printf("%-15s : %s%n".formatted("Target Method",joinPoint.getSignature().getName()));
+		System.out.printf("%-15s : %s%n".formatted("Target Class", joinPoint.getTarget().getClass().getSimpleName()));
+		System.out.printf("%-15s : %s%n".formatted("Target Method", joinPoint.getSignature().getName()));
 		System.out.println("--------------------");
 	}
 
